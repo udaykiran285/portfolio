02f6005f03b36a919d5cf7183915bd9e61dc5ec9
uday <uday837882gmail.com>
2024-07-10 16:05:27 +0530

    private

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -18,7 +18,7 @@ const Home  = () => (
         < Navbar navLinksObject={navLinksObject} />
         <div className='home-bg'>
             <div className='container'>
-            <div className='row d-md-block d-flex  align-items-center pb-4 pl-2'>
+            <div className='row d-md-none pb-4 pl-2'>
                     <div className='col-11 d-md-none d-flex justify-content-center align-items-center'>
                         <img src="https://res.cloudinary.com/djhzxvu0f/image/upload/v1698575490/Grey_Modern_LinkedIn_Profile_Picture_oljnvx.png" className='home-img-sm' alt="profile"/>
                     </div>
@@ -38,11 +38,8 @@ const Home  = () => (
                         <img src="https://res.cloudinary.com/djhzxvu0f/image/upload/v1698575490/Grey_Modern_LinkedIn_Profile_Picture_oljnvx.png" className='home-img' alt="profile"/>
                     </div>
                 </div>
-                <div className='row first-section d-none d-md-block d-flex  align-items-center'>
-                    <div className='col-11 d-md-none d-flex justify-content-center align-items-center'>
-                        <img src="https://res.cloudinary.com/djhzxvu0f/image/upload/v1698575490/Grey_Modern_LinkedIn_Profile_Picture_oljnvx.png" className='home-img-sm' alt="profile"/>
-                    </div>
-                    <div className='intro col-12 col-md-6'>
+                <div className='row first-section d-none d-md-flex align-items-center'>
+                    <div className='intro col-6'>
                         <p className="title">-INTRODUCTION</p>
                         <p className="hi-there">Hi There!👋</p>
                         <h1 className="my-name">I'm <span>Uday Kiran Kuruva</span></h1>
@@ -54,7 +51,7 @@ const Home  = () => (
                             <a href="https://www.linkedin.com/in/uday-kuruvaa" target="_blank" rel="noreferrer"><FaLinkedin className='icon'/></a>
                         </div>       
                     </div>
-                    <div className='col-12 col-md-5 d-none d-md-block'>
+                    <div className='col-5'>
                         <img src="https://res.cloudinary.com/djhzxvu0f/image/upload/v1698575490/Grey_Modern_LinkedIn_Profile_Picture_oljnvx.png" className='home-img' alt="profile"/>
                     </div>
                 </div>
